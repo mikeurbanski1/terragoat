e6a47d6b8c62d217011083a575ffe7c3ea5fae0c
Mike Urbanski <mike@bridgecrew.io>
2020-10-06 08:54:29 -0500

    add dummy bucket

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -149,3 +149,21 @@ resource "aws_s3_bucket" "conf7" {
     }
   }
 }
+
+resource "aws_s3_bucket" "data2" {
+  bucket = "${local.resource_prefix.value}-data2"
+  acl = "public-read"
+  force_destroy = true
+  tags = {
+    Owner = "data-team"
+    Environment = local.resource_prefix.value
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
