ca7f8e4482c81152bb4d19e0c9c9a3dfc4647ac4
Mike Urbanski <mike@bridgecrew.io>
2020-10-06 08:55:51 -0500

    x

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -158,12 +158,4 @@ resource "aws_s3_bucket" "data2" {
     Owner = "data-team"
     Environment = local.resource_prefix.value
   }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 }
