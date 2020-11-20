8c34b7d866cf835a9342de881dd494ff2f390b4b
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2020-11-20 08:45:58 -0600

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -6,14 +6,6 @@ resource "aws_s3_bucket" "data" {
     Owner = "data-team"
     Environment = local.resource_prefix.value
   }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 }
 
 resource "aws_s3_bucket" "data_science" {
