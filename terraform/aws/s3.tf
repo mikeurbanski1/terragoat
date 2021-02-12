00ce0b676c3eaaeebb4fb65ab4ab3725f3422c61
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2021-02-12 17:29:39 -0600

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -7,13 +7,6 @@ resource "aws_s3_bucket" "data" {
     Environment = local.resource_prefix.value
   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 }
 
 resource "aws_s3_bucket" "data_science" {
@@ -21,14 +14,6 @@ resource "aws_s3_bucket" "data_science" {
   acl = "private"
   versioning {
     enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
   }
 
   logging {
